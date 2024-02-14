f2ddd395affce1d57a5adc9bbf45d00e0b8777c6
Woongsik <airwoong83@gmail.com>
2024-02-14 15:49:57 +0200

    Add router to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
-import "./App.css";
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Footer from './components/footer/Footer';
 
 function App() {
-  return <div className="App">App</div>;
-}
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
