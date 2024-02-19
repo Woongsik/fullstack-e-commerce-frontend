d8e7f1733a03701ed7ce94f56ccd7593c0e38065
Woongsik <airwoong83@gmail.com>
2024-02-19 16:59:18 +0200

    Fix models as default

---
+++ src/misc/types/Category.ts
@@ -0,0 +1,9 @@
+type Category = {
+  id: number;
+  name: string;
+  image: string;
+  creationAt: string;
+  updatedAt: string;
+}
+
+export default Category;
--- src/misc/types/Product.ts
+++ src/misc/types/Product.ts
@@ -1,4 +1,14 @@
-export type Product = {
-  id: number,
-  title: string
+import Category from "./Category";
+
+type Product = {
+  id: string;
+  title: string;
+  price: number;
+  description: string;
+  images: string[];
+  creationAt: string;
+  updatedAt: string;
+  category: Category
 }
+
+export default Product;
