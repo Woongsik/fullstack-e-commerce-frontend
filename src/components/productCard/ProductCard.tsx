64cf732408715724ba0f28bc3597278fe75aba74
Woongsik <airwoong83@gmail.com>
2024-03-01 17:13:11 +0200

    Clean up

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -3,9 +3,10 @@ import {
   Typography
 } from '@mui/material';
 
-import UiImage from '../ui/UiImage';
+import UiImage from '../ui/Image/UiImage';
 import { Product } from '../../misc/types/Product';
 import { Link } from 'react-router-dom';
+import UiNoImage from '../ui/Image/UiNoImage';
 
 type Props = {
   product: Product
@@ -20,8 +21,12 @@ export default function ProductCard(props: Props) {
             style={{ textDecoration: 'none', color: 'black'}}>
         <Box component={'div'}>
           <Box component={'div'} width={300} height={300}>
-            <UiImage src={product.images && product.images[0] ? product.images[0] :''}
-                  alt={product.title} />
+            {(product.images && product.images[0]) ?
+              <UiImage 
+                src={product.images && product.images[0] ? product.images[0] :''}
+                alt={product.title} />
+            : <UiNoImage />
+            }
           </Box>
           
           <Box component={'div'}>
