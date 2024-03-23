b8aa1339b1f39a544e4218a9decc2eb8fa2c9857
Woongsik <airwoong83@gmail.com>
2024-03-23 23:48:58 +0200

    Add no items found

--- src/components/product/productList/ProductList.tsx
+++ src/components/product/productList/ProductList.tsx
@@ -1,10 +1,11 @@
-import { 
-  Box,
-  Grid
-} from '@mui/material';
+import { useSelector } from 'react-redux';
+import { Box, CircularProgress, Grid } from '@mui/material';
+import SearchOffIcon from '@mui/icons-material/SearchOff';
 
 import ProductCard from '../productCard/ProductCard';
 import { Product } from '../../../misc/types/Product';
+import { AppState } from '../../../redux/store';
+import CenteredContainer from '../../ui/layout/CenteredContainer';
 
 type Props = {
   products: Product[];
@@ -12,15 +13,27 @@ type Props = {
 
 export default function ProductList(props: Props) {
   const { products } = props;
+  const { minMaxPrice, loading } = useSelector((state: AppState) => state.productReducer)
 
   return (
     <Box component={'div'} margin={1}> 
-      <Grid container spacing={2} justifyContent={'center'}>
+      <Grid container spacing={2} justifyContent={'center'} minHeight={'50vh'}>
         {products.map((product: Product) => 
         <Grid item key={product.id}>
           <ProductCard product={product} />
         </Grid>
         )}
+        {(!loading && minMaxPrice && minMaxPrice.length > 0 && products.length === 0) &&
+          <CenteredContainer>
+            <SearchOffIcon sx={{ fontSize: 35, marginRight: 1 }} />
+            <h2> No items found!</h2> 
+          </CenteredContainer>
+        }
+        { loading && 
+          <CenteredContainer>
+            <CircularProgress sx={{ fontSize: 35 }}/>
+          </CenteredContainer>
+        }
       </Grid>
     </Box>
   )
